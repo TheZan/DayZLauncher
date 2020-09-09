2d5cdad6ca607da1f2cdf814a0b473d875ca19fa
TheZan <sochnev66@gmail.com>
2020-09-09 17:35:02 +0300

    Add parameters and mods page design

---
+++ DayZLauncher/Model/Mod.cs
@@ -0,0 +1,9 @@
+﻿namespace DayZLauncher.Model
+{
+    public class Mod
+    {
+        public string Name { get; set; }
+        public bool IsSelectedMod { get; set; }
+        public string SteamUrl { get; set; }
+    }
+}
--- DayZLauncher/ViewModel/ModsPageViewModel.cs
+++ DayZLauncher/ViewModel/ModsPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using DayZLauncher.Navigation;
+﻿using DayZLauncher.Model;
+using DayZLauncher.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +8,27 @@ namespace DayZLauncher.ViewModel
 {
     public class ModsPageViewModel : BaseViewModel, IPageViewModel
     {
+        public ModsPageViewModel()
+        {
+            Mods = new List<Mod>()
+            {
+                new Mod(){Name = "@COMMUNITYFRAMEWORK", IsSelectedMod = false},
+                new Mod(){Name = "@COMMUNITYONLINETOOLS", IsSelectedMod = false},
+                new Mod(){Name = "@DAYZEXPANSION", IsSelectedMod = true},
+                new Mod(){Name = "@DAYZEXPANSIONLICENSED", IsSelectedMod = false}
+            };
+        }
+
+        private List<Mod> mods;
+
+        public List<Mod> Mods
+        {
+            get => mods;
+            set
+            {
+                mods = value;
+                OnPropertyChanged("Mods");
+            }
+        }
     }
 }
